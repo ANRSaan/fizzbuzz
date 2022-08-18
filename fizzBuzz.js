8a13d439afe20f7c6bedd73b892468d27cb89dae
Brian Williams <heffergod@gmail.com>
2022-08-18 09:21:34 -0700

    Tried taking out brackets, which was interesting

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -2,18 +2,15 @@
 for (i = 1; i <= 50; i++)
 {
     if (i % 3 === 0 && i % 5 === 0)
-    {
         console.log("fizzbuz")
-    }
+    
     else if (i % 3 === 0)
-    {
         console.log("fizz")
-    }
+
     else if (i % 5 === 0)
-    {
         console.log("buzz")
-    }
-    else{
+
+    else
         console.log(i)
-    }
+    
 }
